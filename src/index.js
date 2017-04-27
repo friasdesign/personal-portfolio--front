cad7972c7acef912b073681b064af268929fd924
Carlos Frias <carlosfrias@Carloss-MacBook-Air.local>
2017-04-27 10:36:41 -0300

    Fix issues with styles

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+// ASSETS _____________________________________________________________________
+import './assets/normalize.css'
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
