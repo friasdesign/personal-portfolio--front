c7199c8b75e42f60a5058954fb7dd0ccfacad21f
Carlos Frias <carlosfrias@Carloss-MacBook-Air.local>
2017-05-23 11:07:42 -0300

    Add behavior to Navbar for prototyping

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -38,30 +38,47 @@ class Navbar extends Component {
    )
  }
 
+  // TODO: refactor to better syntax
   render() {
     const {navOpen} = this.state
-    const {
-      addMotionToComponent
-    } = this
+    const {addMotionToComponent} = this
+    const {minimal} = this.props
+
+    const minimalStyle = minimal ? {display: 'block'} : {}
+
     return (
       <FadeIn
         onRest={f => f}
       >
         <header role="banner"
-          className={`top-nav ${navOpen ? 'top-nav--open' : ''}`}
+          className={`top-nav ${navOpen ? 'top-nav--open' : ''}
+            ${minimal ? 'top-nav--minimal' : ''}
+          `}
           >
           <div className="top-nav__on-top">
-            <div className="logo-container">
-              <img src={logo} alt="logo" className="logo" />
-            </div>
-            <div className="hamburger" onClick={this.toggleMenuHandler.bind(this)}>
+            {
+              minimal
+              ? null
+              : (
+                <div className="logo-container">
+                  <img src={logo} alt="logo" className="logo" />
+                </div>
+              )
+            }
+            <div className="hamburger" onClick={this.toggleMenuHandler.bind(this)}
+              style={minimalStyle}
+            >
               {
                 navOpen
                 ? addMotionToComponent(Cross, 1)
                 : addMotionToComponent(Hamburger, 2)
               }
             </div>
-            <List topNav={true} />
+            {
+              minimal
+              ? null
+              : <List topNav={true} />
+            }
           </div>
           {
             navOpen
