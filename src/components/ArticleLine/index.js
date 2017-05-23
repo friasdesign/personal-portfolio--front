2bf052bd4be35e19c53bdcc7857e669e5ad0acd0
Carlos Frias <carlosfrias@Carloss-MacBook-Air.local>
2017-05-23 15:19:12 -0300

    Add styles for article-entries

--- src/components/ArticleLine/index.js
+++ src/components/ArticleLine/index.js
@@ -2,6 +2,8 @@
 
 import React from 'react'
 
+import './ArticleLine.sass'
+
 type ArticleLineProps = {
   heading: string,
   entries: Array<{
@@ -15,24 +17,26 @@ type ArticleLineProps = {
 const ArticleLine = ({heading, entries}: ArticleLineProps) => (
   <article className="article-line">
     <h2 className="article-heading">{heading}</h2>
-    {
-      entries.map((e, i) => (
-        <div key={i} className={`line__entry ${
-            i % 2 === 0
-            ? 'line__entry--even'
-            : ''
-          }`}>
-          <img src={e.logo} alt={`Logo of ${e.h}`} className="entry__logo"/>
-          <div className="entry__content">
-            <p className="entry__sub">{e.sub}</p>
-            <h3 className="entry__h">{e.h}</h3>
-            <p className="entry__text"
-              dangerouslySetInnerHTML={{__html: e.text}}
-            />
+    <div className="line__entries">
+      {
+        entries.map((e, i) => (
+          <div key={i} className={`line__entry ${
+              i % 2 !== 0
+              ? 'line__entry--odd'
+              : ''
+            }`}>
+            <img src={e.logo} alt={`Logo of ${e.h}`} className="entry__logo"/>
+            <div className="entry__content">
+              <p className="entry__sub">{e.sub}</p>
+              <h3 className="entry__h">{e.h}</h3>
+              <p className="entry__text"
+                dangerouslySetInnerHTML={{__html: e.text}}
+              />
+            </div>
           </div>
-        </div>
-      ))
-    }
+        ))
+      }
+    </div>
   </article>
 )
 
