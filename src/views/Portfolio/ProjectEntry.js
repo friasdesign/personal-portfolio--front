e3baecf12966a8c38c02388a086ff15fbe4bef5d
Carlos Frias <carlosfrias@Carloss-MacBook-Air.local>
2017-06-12 13:21:48 -0300

    Add animation to project entry

--- src/views/Portfolio/ProjectEntry.js
+++ src/views/Portfolio/ProjectEntry.js
@@ -6,12 +6,15 @@ import './ProjectEntry.sass'
 
 // COMPONENTS __________________________________________________________________
 import LinkIcon from '../../components/LinkIcon'
+import TriggerOnScreen from '../../components/TriggerOnScreen'
 
 // TYPE ________________________________________________________________________
 import type {ProjectType} from './_data'
 
 type ProjectEntryProps = {
-  project: ProjectType
+  project: ProjectType,
+  setTopPosition: () => void,
+  triggered: boolean
 }
 
 const ProjectEntry = (props: ProjectEntryProps) => {
@@ -23,50 +26,60 @@ const ProjectEntry = (props: ProjectEntryProps) => {
     techs,
     links
   } = props.project
+  const {
+    setTopPosition,
+    triggered
+  } = props
   return (
-    <div className="project-entry">
-      <div className="project__img-container">
-        <img srcSet={`
-            ${images.x2} 2x,
-            ${images.x3} 3x,
-          `}
-          className="project__img"
-          src={images.default}
-          alt={`
-              Snapshot of project ${title}
+    <div ref={e => setTopPosition(e)} className="project-entry">
+      <div className={`project-entry-container fade-bottom ${
+          triggered
+          ? ''
+          : 'fade-bottom--init'
+        }`}>
+        <div className="project__img-container">
+          <img srcSet={`
+              ${images.x2} 2x,
+              ${images.x3} 3x,
             `}
-        />
-      </div>
-      <article className="project__text">
-        <header className="project__header">
-          <h3 className="project__title">{title}</h3>
-          <div className="project__type">
-            <span className="type__text">{type}</span>
-          </div>
-        </header>
-        <p className="project__description">{description}</p>
-        <footer className="icon-container project__footer">
-          <div className="project__techs">
-            <h4 className="project__footer__heading techs">Technologies</h4>
-            {
-              techs.map(({title, logo}, i) =>
-                <img className="project__icon"
-                  key={i} title={title} src={logo} alt={title} />
-              )
-            }
-          </div>
-          <div className="project__links">
-            <h4 className="project__footer__heading links">Links</h4>
+            className="project__img"
+            src={images.default}
+            alt={`
+                Snapshot of project ${title}
+              `}
+          />
+        </div>
+        <article className="project__text">
+          <header className="project__header">
+            <h3 className="project__title">{title}</h3>
+            <div className="project__type">
+              <span className="type__text">{type}</span>
+            </div>
+          </header>
+          <p className="project__description">{description}</p>
+          <footer className="icon-container project__footer">
+            <div className="project__techs">
+              <h4 className="project__footer__heading techs">Technologies</h4>
               {
-                links.map(({title, link, type}, i) =>
-                  <LinkIcon key={i} link={link} title={title} logo={type}/>
+                techs.map(({title, logo}, i) =>
+                  <img className="project__icon"
+                    key={i} title={title} src={logo} alt={title} />
                 )
               }
-          </div>
-        </footer>
-      </article>
+            </div>
+            <div className="project__links">
+              <h4 className="project__footer__heading links">Links</h4>
+                {
+                  links.map(({title, link, type}, i) =>
+                    <LinkIcon key={i} link={link} title={title} logo={type}/>
+                  )
+                }
+            </div>
+          </footer>
+        </article>
+      </div>
     </div>
   )
 }
 
-export default ProjectEntry
+export default TriggerOnScreen(ProjectEntry)
