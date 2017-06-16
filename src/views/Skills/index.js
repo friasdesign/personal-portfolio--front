70995c09f99bd927a430b0b70aff04e6c3a8cade
Carlos Frias <carlosfrias@Carloss-MacBook-Air.local>
2017-06-16 16:52:34 -0300

    Refactor Skills using TextArticle

--- src/views/Skills/index.js
+++ src/views/Skills/index.js
@@ -7,6 +7,7 @@ import headingImg from './_assets/heading.svg'
 // COMPONENTS __________________________________________________________________
 import ArticleLine from '../../components/ArticleLine'
 import PageTemplate from '../../components/PageTemplate'
+import TextArticle from '../../components/TextArticle'
 
 // DATA ________________________________________________________________________
 import {
@@ -19,31 +20,20 @@ import {
 // COMPONENT DEFINITION ________________________________________________________
 const Skills = ({ready}: {ready: boolean}) => (
   <div className="template-container over-x-hidden">
-    <article className="m-btm-2">
-      <h2 className={`article-heading fade-side ${
-          ready
-          ? ''
-          : 'fade-left'
-        }`}>Levels</h2>
-      <div className={`article-text fade-side ${
-          ready
-          ? ''
-          : 'fade-right'
-        }`}>
-        <p>
-          I have divided my skills into different levels of proficiency, those are, from most proficient to least proficient:
-        </p>
-        <ul className="text-list">
-          <li><strong>Ninja</strong>. A nerd master of the geek arts.</li>
-          <li><strong>Proficient</strong>. Can use that technology under a tight schedule, without too much research needed.</li>
-          <li><strong>Jargon</strong>. Understand all domain-specific lingo, and use it regularly, but feel better talking about it than doing it.</li>
-          <li><strong>Wannabe</strong>. One day I wan’t to become a Ninja of this.</li>
-        </ul>
-        <p>
-          I changed the levels names for human languages, but those used are self explanatory.
-        </p>
-      </div>
-    </article>
+    <TextArticle heading="Levels" ready={ready}>
+      <p>
+        I have divided my skills into different levels of proficiency, those are, from most proficient to least proficient:
+      </p>
+      <ul className="text-list">
+        <li><strong>Ninja</strong>. A nerd master of the geek arts.</li>
+        <li><strong>Proficient</strong>. Can use that technology under a tight schedule, without too much research needed.</li>
+        <li><strong>Jargon</strong>. Understand all domain-specific lingo, and use it regularly, but feel better talking about it than doing it.</li>
+        <li><strong>Wannabe</strong>. One day I wan’t to become a Ninja of this.</li>
+      </ul>
+      <p>
+        I changed the levels names for human languages, but those used are self explanatory.
+      </p>
+    </TextArticle>
 
     <ArticleLine
       className={`fade-side ${
