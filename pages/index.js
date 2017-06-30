53fd08680af86f37902976011654832fece038b9
Carlos Frias <carlosfrias@Carloss-MacBook-Air.local>
2017-06-30 19:52:43 -0300

    Fix issue with curtain order

--- pages/index.js
+++ pages/index.js
@@ -18,10 +18,10 @@ import Introduction from '../components/Introduction'
 const Index = (props: any) => (
   <main className="home">
     <Globals/>
-    <div className="home-content home-content--pic">
-      <h1 id="home__title" hidden>Home</h1>
-      <div className="curtain">
-        <Introduction/>
+    <div className="curtain">
+      <div className="home-content home-content--pic">
+        <h1 id="home__title" hidden>Home</h1>
+          <Introduction/>
       </div>
     </div>
 
