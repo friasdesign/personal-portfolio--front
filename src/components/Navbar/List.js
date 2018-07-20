41978cbf5bafa0669771b5eef4b1d7cce0f1f3d4
Carlos Frias <carlos.a.frias@gmail.com>
2018-07-20 12:05:46 -0300

    Fix navbar issues

--- src/components/Navbar/List.js
+++ src/components/Navbar/List.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import {StaggeredMotion, spring} from 'react-motion'
-import {NavLink} from 'react-router-dom'
+import { StaggeredMotion, spring } from 'react-motion'
+import { NavLink } from 'react-router-dom'
 
 const MENU_ITEMS_TOP = [
   {
@@ -36,39 +36,43 @@ function TopList() {
   return (
     <ul className="nav__list top-nav__list">
       {
-        MENU_ITEMS_TOP.map(({link, name}, i) => (
-          <li key={i}><a href={link} className="top-nav__link">{name}</a></li>
+        MENU_ITEMS_TOP.map(({ link, name }, i) => (
+          <li key={i}>
+            <NavLink to={link} className="top-nav__link">
+              {name}
+            </NavLink>
+          </li>
         ))
       }
     </ul>
   )
 }
 
-function List({topNav}) {
-  if(topNav) return TopList()
+function List({ topNav }) {
+  if (topNav) return TopList()
 
   return (
     <StaggeredMotion
-      defaultStyles={MENU_ITEMS.map((item, i) => ({x: 100, o: 0}))}
+      defaultStyles={MENU_ITEMS.map((item, i) => ({ x: 100, o: 0 }))}
       styles={prevInterpolatedStyles => prevInterpolatedStyles.map((_, i) => {
         return i === 0
           // Initial stiffness and damping
           ? {
-              x: spring(0, {stiffness: STIFFNESS, damping: DAMPING}),
-              o: spring(1)
-            }
+            x: spring(0, { stiffness: STIFFNESS, damping: DAMPING }),
+            o: spring(1)
+          }
           // Final stiffness and damping
           : {
-              x: spring(prevInterpolatedStyles[i - 1].x, {
-                stiffness: STIFFNESS, damping: DAMPING
-              }),
-              o: spring(prevInterpolatedStyles[i - 1].o)
-            };
+            x: spring(prevInterpolatedStyles[i - 1].x, {
+              stiffness: STIFFNESS, damping: DAMPING
+            }),
+            o: spring(prevInterpolatedStyles[i - 1].o)
+          };
       })}
     >
       {interpolatingStyles =>
         <ul className="nav__list">
-          {interpolatingStyles.map(({x, o}, i) => {
+          {interpolatingStyles.map(({ x, o }, i) => {
             const styles = {
               transform: `translate(${x}px, 0)`,
               opacity: o
